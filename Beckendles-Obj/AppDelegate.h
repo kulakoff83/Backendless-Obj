e160916078b732663fc6c1e2157ad1e58c8e6faf
Dmitry Kulakov <kulakoff@kilograpp.com>
2017-01-25 17:53:18 +0200

    Initial Commit

---
+++ Beckendles-Obj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Beckendles-Obj
+//
+//  Created by Dmitry Kulakov on 25.01.17.
+//  Copyright © 2017 kulakoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Beckendles-Obj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Beckendles-Obj
+//
+//  Created by Dmitry Kulakov on 25.01.17.
+//  Copyright © 2017 kulakoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
